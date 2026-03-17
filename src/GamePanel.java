d1cb4fa4a5cabe7bc0321e1ea04eb4f4939c0e5e
BrotherJorg <243885350+BrotherJorg@users.noreply.github.com>
2026-03-17 09:44:59 +0800

    added unit class for adding units

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,28 +2,53 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
+    Unit unitA;
+    Unit unitB;
+
+    GamePanel() {
+    unitA = new Unit(0, 0);
+     unitB = new Unit(2, 1);
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-       // g.setColor(Color.BLACK);
+       g.setColor(Color.BLACK);
        // g.fillRect(50,50,50,50);
        
-     int tilesizex = 50;
-     int tilesizey = 50;
-     int tilecorx = 50;
-     int tilecory = 50;
+     int tilesizeX = 50;
+     int tilesizeY = 50;
+     int tilecorX = 50;
+     int tilecorY = 50;
      int grid = 5;
      int gap = 2;
 
+
      for (int i = 0; i < grid; i++) {
-       int  xs = tilecorx + i * tilesizex;
+       int  xs = tilecorX + i * tilesizeX;
        
         for (int j = 0; j< grid; j++ ) {
-            int ys = tilecory + j * tilesizey;
-          g.fillRect(xs, ys, tilesizex -gap, tilesizey- gap);
+            int ys = tilecorY + j * tilesizeY;
+          g.fillRect(xs, ys, tilesizeX -gap, tilesizeY- gap);
             
         }
      }
+
+    g.setColor(Color.BLUE);
+     int pixelX = tilecorX + unitA.gridX * tilesizeX;
+     int pixelY = tilecorY + unitA.gridY * tilesizeY;
+      g.fillRect(pixelX, pixelY, tilesizeX - gap, tilesizeY - gap);
+    
+  g.setColor(Color.RED);
+     pixelX = tilecorX + unitB.gridX * tilesizeX;
+     pixelY = tilecorY + unitB.gridY * tilesizeY;
+g.fillRect(pixelX, pixelY, tilesizeX - gap, tilesizeY - gap);
+
+
+
+ 
+  
+
 
         
 }
---
+++ src/Unit.java
@@ -0,0 +1,11 @@
+public class Unit {
+    int gridX = 0;
+    int gridY = 0;
+
+    
+    public Unit(int gridX, int gridY) {     
+        this.gridX = gridX;
+        this.gridY = gridY;
+    }
+  
+    }
